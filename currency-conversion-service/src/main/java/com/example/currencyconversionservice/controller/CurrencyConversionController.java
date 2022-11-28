30d6bd50de11d061454ac2e89df533a593f0fc91
jasoncc95 <jasoncc95@gmail.com>
2022-11-28 14:58:12 -0600

    Retrieving data from currency-exchange-service and making calculation

--- currency-conversion-service/src/main/java/com/example/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/example/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,18 +1,31 @@
 package com.example.currencyconversionservice.controller;
 
 import com.example.currencyconversionservice.model.CurrencyConversion;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
-        return new CurrencyConversion(10001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate()
+                .getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
+        CurrencyConversion currencyConversion = responseEntity.getBody();
+        if (currencyConversion == null) {
+            throw new RuntimeException(String.format("Could not get the data for from: %s to: %s", from, to));
+        }
+        currencyConversion.setTotalCalculatedAmount(quantity.multiply(currencyConversion.getConversionMultiple()));
+        return currencyConversion;
     }
 
 }
