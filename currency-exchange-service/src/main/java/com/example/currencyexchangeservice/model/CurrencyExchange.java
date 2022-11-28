0467bd7cd2e96a0d166c76452b35de683cf7e086
jasoncc95 <jasoncc95@gmail.com>
2022-11-28 11:49:34 -0600

    Adding h2 db and some data for currency exchange

--- currency-exchange-service/src/main/java/com/example/currencyexchangeservice/model/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/example/currencyexchangeservice/model/CurrencyExchange.java
@@ -1,10 +1,18 @@
 package com.example.currencyexchangeservice.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 import java.math.BigDecimal;
 
+@Entity(name = "currency_exchange")
 public class CurrencyExchange {
+    @Id
     private Long id;
+    @Column(name = "currency_from")
     private String from;
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private String environment;
