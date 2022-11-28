3e5a12d4f3cadf9d21ca3225ec4b19c17d8fa569
jasoncc95 <jasoncc95@gmail.com>
2022-11-28 11:33:44 -0600

    Adding port in response for CurrencyExchangeService

--- currency-exchange-service/src/main/java/com/example/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/example/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,6 +1,7 @@
 package com.example.currencyexchangeservice.controller;
 
 import com.example.currencyexchangeservice.model.CurrencyExchange;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +10,17 @@ import java.math.BigDecimal;
 
 @RestController()
 public class CurrencyExchangeController {
+    private final Environment environment;
+
+    public CurrencyExchangeController(Environment environment) {
+        this.environment = environment;
+    }
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange getExchangeValue(@PathVariable String from, @PathVariable String to) {
-        return new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+        currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
+        return currencyExchange;
     }
 
 }
