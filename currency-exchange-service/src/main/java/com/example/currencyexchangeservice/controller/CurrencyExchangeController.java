98105726ba221d37de9583a5a0f0d52969006c25
jasoncc95 <jasoncc95@gmail.com>
2022-11-28 14:14:43 -0600

    Adding JPA repository to read from h2 db

--- currency-exchange-service/src/main/java/com/example/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/example/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,26 +1,33 @@
 package com.example.currencyexchangeservice.controller;
 
 import com.example.currencyexchangeservice.model.CurrencyExchange;
+import com.example.currencyexchangeservice.repository.CurrencyExchangeRepository;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 @RestController()
 public class CurrencyExchangeController {
     private final Environment environment;
+    private final CurrencyExchangeRepository currencyExchangeRepository;
 
-    public CurrencyExchangeController(Environment environment) {
+    public CurrencyExchangeController(Environment environment, CurrencyExchangeRepository currencyExchangeRepository) {
         this.environment = environment;
+        this.currencyExchangeRepository = currencyExchangeRepository;
     }
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange getExchangeValue(@PathVariable String from, @PathVariable String to) {
-        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
-        currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
-        return currencyExchange;
+        Optional<CurrencyExchange> currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
+        if (currencyExchange.isEmpty()) {
+            throw  new RuntimeException(String.format("Could not find data for from: %s to: %s", from, to));
+        }
+        currencyExchange.get().setEnvironment(environment.getProperty("local.server.port"));
+        return currencyExchange.get();
     }
 
 }
