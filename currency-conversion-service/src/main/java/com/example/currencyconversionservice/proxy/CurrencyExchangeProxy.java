1b02a5481c45858230493e1b0da7e78d8a40c52c
jasoncc95 <jasoncc95@gmail.com>
2022-11-28 22:12:45 -0600

    Allowing feign client to load balance the request between the available currency-exchange-service instances

--- currency-conversion-service/src/main/java/com/example/currencyconversionservice/proxy/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/example/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -5,7 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+@FeignClient(name = "currency-exchange-service")    //with this, the naming-server will load balance between the available instances of the service
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
